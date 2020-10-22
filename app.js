53e90b3c751d05a4c028dd1adeed9e21770b6e97
mehdi-cloud <mehdikamal1999@gmail.com>
2020-10-22 00:55:13 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const burger=  document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach(link =>{
+            if(link.style.animation){
+                link.style.animation = '';
+            } else{ 
+                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 +1.5}s`;
+            }
+        });
+    });
+
+}
+
+navSlide();
